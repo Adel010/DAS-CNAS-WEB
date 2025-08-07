66214da08da61834ba8056e86f7947d74c0aea81
Adel Alikeche <adel.alikeche.pro@gmail.com>
2025-08-07 23:29:22 +0100

    creat Employeur form

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Let's get DAS CNAS web version done!");
+
+const today = new Date();
+document.getElementById("annee-reference").value = Number(today.getFullYear())-1;
+document.getElementById("date-depot").value = String(today.getFullYear()) + "-"  + String((today.getMonth() + 1 )).padStart(2,"0") + "-" + String(today.getDate()).padStart(2,"0") ;
